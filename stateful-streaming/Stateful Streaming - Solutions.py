a7a9ca6318aa2e065c185faffc392466a6edc6d4
Syed <masroor.rizvi@gmail.com>
2023-05-04 21:49:40 +0530

    #10 | Added the latest notebook

--- stateful-streaming/Stateful Streaming - Solutions.py
+++ stateful-streaming/Stateful Streaming - Solutions.py
@@ -45,18 +45,30 @@ helpers.clean_working_directory()
 
 # COMMAND ----------
 
-import pandas as pd
-from pyspark.sql.functions import monotonically_increasing_id
-
-
-url = "https://raw.githubusercontent.com/kelseymok/charge-point-live-status/main/ocpp_producer/data/1683036538.json"
-pandas_df = pd.read_json(url, orient='records')
-mock_data_df = spark.createDataFrame(pandas_df).withColumn("temp_index", monotonically_increasing_id())
 
 
 # COMMAND ----------
 
-print(df)
+import pandas as pd
+from pyspark.sql.functions import col,lit
+
+url = "https://raw.githubusercontent.com/kelseymok/charge-point-live-status/main/ocpp_producer/data/1683036538.json"
+pandas_df = pd.read_json(url, orient='records')
+pandas_df["index"] = pandas_df.index
+mock_data_df = spark.createDataFrame(pandas_df)
+
+
+# COMMAND ----------
+
+print(mock_data_df)
+
+# COMMAND ----------
+
+display(mock_data_df.select("index"))
+
+# COMMAND ----------
+
+display(mock_data_df)
 
 # COMMAND ----------
 
@@ -81,12 +93,95 @@ raw_stream_data = (
 )
 
 # This is just data setup, not part of the exercise
-raw_stream_data.\
-    join(mock_data_df, raw_stream_data.value == mock_data_df.temp_index, 'left').\
+joined_df = raw_stream_data.\
+    join(mock_data_df, raw_stream_data.value == mock_data_df.index, 'left').\
     drop("timestamp").\
-    drop("temp_index")
+    drop("index")
 
-## I really hope that this joins properly, could not test.
+# I really hope that this joins properly, could not test.
+
+# COMMAND ----------
+
+# Extract fields from the nested body column and get that in the extracted dataframe
+
+from pyspark.sql import DataFrame
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StringType, IntegerType, ArrayType, DoubleType, LongType
+
+body_schema = StructType([
+    StructField("connector_id", IntegerType(), True),
+    StructField("error_code", StringType(), True),
+    StructField("status", StringType(), True),
+    StructField("timestamp", StringType(), True),
+    StructField("info", IntegerType(), True),
+    StructField("vendor_id", IntegerType(), True),
+    StructField("vendor_error_code", IntegerType(), True),
+])
+
+exploded_df = joined_df.withColumn("new_body", from_json(col("body"), schema=body_schema))
+
+
+
+
+# COMMAND ----------
+
+
+# Let us now filter the exploded dataframe above to get status and timestamp from the body field
+# We are also dropping fields body, new_body, value etc which are not required
+# The resulting dataframe essentially will just have 3 field remaining i.e charge_point_id, status and timestamp
+
+from pyspark.sql.functions import col, expr, to_timestamp
+import pyspark.sql.functions as F
+
+filtered_df = exploded_df \
+    .withColumn("status", expr("new_body.status"))\
+    .withColumn("timestamp", to_timestamp(col("new_body.timestamp")))\
+    .drop("body")\
+    .drop("new_body")\
+    .drop("value")\
+    .drop("action")\
+    .drop("message_type")
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import from_json, window
+
+# This is the real meat of the logic where we groupBy charge_point_id, status and Time Window
+# We then aggregate it to get the status count in a particular time window
+agg_df = filtered_df\
+    .withWatermark("timestamp", "10 minute") \
+    .groupBy(col("charge_point_id").alias("Charge Point Id"),
+             col("status").alias("Latest Status"),
+             window(col("timestamp"), "5 minute").alias("Time Window"))\
+    .agg(F.count(col("status")).alias("Status Count"))
+
+
+# COMMAND ----------
+
+query = agg_df \
+    .writeStream \
+    .outputMode("update") \
+    .format("memory") \
+    .queryName("counts")\
+    .option('truncate', 'false') \
+    .start()
+
+
+# COMMAND ----------
+
+# MAGIC %sql select `Time Window`, `Charge Point Id`, `Status Count` from counts;
+
+# COMMAND ----------
+
+# MAGIC %sql select date_format(`Time Window`.end, "MMM-dd HH:mm") as time from counts
+
+# COMMAND ----------
+
+
+
+display(agg_df)
 
 # COMMAND ----------
 
